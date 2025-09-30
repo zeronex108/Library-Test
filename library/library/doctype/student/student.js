797beada980d2055c6e2eff61385274785a53275
Pankaj Patel <panky.webdev@gmail.com>
2025-09-30 17:33:58 +0000

    doctype update

---
+++ library/library/doctype/student/student.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Pankaj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library/library/doctype/student/test_student.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Pankaj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStudent(FrappeTestCase):
+	pass
